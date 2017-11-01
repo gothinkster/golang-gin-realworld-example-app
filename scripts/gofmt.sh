20536c44d03f4e9a20aabc99bc96d18e9c65a371
wangzitian0 <zitwang@linkedin.com>
2017-11-01 19:23:53 +0800

    add code go fmt

--- scripts/gofmt.sh
+++ scripts/gofmt.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-if [ -n "$(go fmt ./...)" ]; then
-    echo "Go code is not formatted:"
-    gofmt -d .
+if [[ -n "$(go fmt ./...)" ]]; then
+    echo "There is unformatted code, you should use `go fmt ./...` to format it"
     exit 1
 fi
