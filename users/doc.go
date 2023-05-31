b803752142af36c8163a130734748ac1395e3c01
Soufiane <soufiantamasi@gmail.com>
2023-05-31 13:54:53 +0100

    Add unit_test documentation to the doc.go of the users package

--- users/doc.go
+++ users/doc.go
@@ -8,5 +8,8 @@ routers.go: router binding and core logic
 serializers.go: definition the schema of return data
 
 validators.go: definition the validator of form data
+
+unit_test.go: contains unit tests for the module
 */
+
 package users
